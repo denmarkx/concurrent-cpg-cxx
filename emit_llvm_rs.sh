8e2b67cebfc59f093282f3c8abec0ea1d7eec946
Mark <frozenstranger510@gmail.com>
2026-03-11 23:31:19 -0500

    passes: eliminate CEP (passes weren't actually being fully run through -Cpasses)

--- emit_llvm_rs.sh
+++ emit_llvm_rs.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 rustc\
     --emit=llvm-ir\
     -Copt-level=0\
@@ -6,19 +7,11 @@ rustc\
     -Cdebug-assertions=off\
     -Clto=fat\
     -Cllvm-args="--disable-i2p-p2i-opt --emit-call-site-info"\
-    -Cpasses="internalize\
-    canonicalize-aliases \
-    strip-dead-prototypes \
-    strip-dead-debug-info \
-    constmerge \
-    globaldce \
-    globalopt \
-    adce \
-    simplifycfg \
-    instsimplify \
-    instnamer \
-    called-value-propagation \
-    function-attrs \
-    dce"\
     "std_rs.rs"\
+    -o "std_rs.ll"
+
+opt-17 \
+    -S \
+    --passes="called-value-propagation,mem2reg,early-cse,canonicalize-aliases,loop-simplify,lcssa,loop-rotate,loop-instsimplify,indvars,loop-deletion,loop-reduce,correlated-propagation,instcombine,jump-threading,simplifycfg,tailcallelim,reassociate,gvn,sccp,instcombine,function-attrs,instcombine,adce,dce,globalopt,globaldce,constmerge,strip-dead-prototypes,strip-dead-debug-info,partially-inline-libcalls" \
+    "std_rs.ll" \
     -o "std_rs.ll"
--- use_ce_pass.sh
+++
@@ -1,1 +0,0 @@
-opt-17 -load-pass-plugin libCEPass.so test_ce.ll -passes="code-elim" -o test_ce.bc
