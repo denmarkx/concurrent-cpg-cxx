13e46b1837aeec25e2d56970a269c5ca45120240
Mark <frozenstranger510@gmail.com>
2026-05-14 21:16:50 -0500

    andersen: cleaner approach at handling memcpy.

--- andersen/NodeFactory.h
+++ andersen/NodeFactory.h
@@ -163,7 +163,11 @@ public:
   NodeIndex getReturnNodeFor(const Context *context, const llvm::Function *f) const;
   NodeIndex getVarargNodeFor(const llvm::Function *f) const;
 
+  // [deprecated] - use lookupFields
   llvm::SmallVector<unsigned int, 4> getFields(const Context *ctx, const llvm::Value *v) const;
+
+  std::vector<FieldType> lookupFields(AndersNode::AndersNodeType type, const Context *ctx, const llvm::Value *v) const;
+
 
   // Node merge interfaces
   void mergeNode(NodeIndex n0, NodeIndex n1); // Merge n1 into n0
